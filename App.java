566cf6874fce2d2feea28a22f3893b6837818f08
carsdfj <carsdfj@gmail.com>
2020-12-03 20:02:12 -0400

    Comienzo de prueba

---
+++ App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) {
+
+
+
+        logger.log("Hello Work");
+
+       // System.out.println("Hello, World!");
+      //  System.out.println("This is  a test with my program");
+    
+    
+    
+    }
+}
